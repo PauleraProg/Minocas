300bbf732c97491bb10cfb68e2c91340d5f2e3af
PauleraProg <pdietrichfilho1@gmail.com>
2025-05-14 21:41:04 -0400

    ButtonsFix

--- Assets/Scripts/IconControl.cs
+++ Assets/Scripts/IconControl.cs
@@ -35,15 +35,56 @@ public class IconControl : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+               icons[currentIndex].gameObject.SetActive(false);
+               currentIndex++;
+               if(currentIndex > 3)
+               {
+                        currentIndex = 0;
+               }
+               icons[currentIndex].gameObject.SetActive(true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
                 icons[currentIndex].gameObject.SetActive(false);
-                currentIndex++;
+                currentIndex--;
+                if (currentIndex < 0)
+                {
+                    currentIndex = 3;
+                }
                 icons[currentIndex].gameObject.SetActive(true);
             }
         }
 
         if (gameObject.CompareTag("P2"))
         {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                icons[currentIndex].gameObject.SetActive(false);
+                currentIndex++;
+                icons[currentIndex].gameObject.SetActive(true);
 
+                if (currentIndex >= 4)
+                {
+                    icons[currentIndex].gameObject.SetActive(false);
+                    currentIndex = 0;
+                    icons[currentIndex].gameObject.SetActive(true);
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                icons[currentIndex].gameObject.SetActive(false);
+                currentIndex--;
+                icons[currentIndex].gameObject.SetActive(true);
+
+                if (currentIndex <= -1)
+                {
+                    icons[currentIndex].gameObject.SetActive(false);
+                    currentIndex = 0;
+                    icons[currentIndex].gameObject.SetActive(true);
+                }
+            }
         }
     }
 }
--- Assets/Scripts/SpellHolder.cs
+++ Assets/Scripts/SpellHolder.cs
@@ -6,12 +6,10 @@ using UnityEngine.UI;
 public class WeaponHolder : MonoBehaviour
 {
     int totalSpells;
-    int totalIcons;
 
     int currentIndex;
 
     public GameObject[] spells;
-    public Image[] icons; 
 
     public GameObject currentSpell;
 
@@ -43,6 +41,7 @@ public class WeaponHolder : MonoBehaviour
     private void Update()
     {
         Switching();
+        NewEquip();
     }
 
     public void Fireball()
@@ -88,5 +87,71 @@ public class WeaponHolder : MonoBehaviour
             casts[1].SetActive(true);
         }
     }
+
+    public void NewEquip()
+    {
+        if (gameObject.CompareTag("P1"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                spells[currentIndex].gameObject.SetActive(false);
+                currentIndex++;
+                spells[currentIndex].gameObject.SetActive(true);
+
+                if (currentIndex >= spells.Length && Input.GetKeyDown(KeyCode.E))
+                {
+                    spells[currentIndex].gameObject.SetActive(false);
+                    currentIndex = 0;
+                    spells[currentIndex].gameObject.SetActive(true);
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                spells[currentIndex].gameObject.SetActive(false);
+                currentIndex--;
+                spells[currentIndex].gameObject.SetActive(true);
+
+                if (currentIndex <= spells.Length && Input.GetKeyDown(KeyCode.Q))
+                {
+                    spells[currentIndex].gameObject.SetActive(false);
+                    currentIndex = 0;
+                    spells[currentIndex].gameObject.SetActive(true);
+                }
+            }
+        }
+
+        if (gameObject.CompareTag("P2"))
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                spells[currentIndex].gameObject.SetActive(false);
+                currentIndex++;
+                spells[currentIndex].gameObject.SetActive(true);
+
+                if (currentIndex >= spells.Length && Input.GetKeyDown(KeyCode.E))
+                {
+                    spells[currentIndex].gameObject.SetActive(false);
+                    currentIndex = 0;
+                    spells[currentIndex].gameObject.SetActive(true);
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                spells[currentIndex].gameObject.SetActive(false);
+                currentIndex--;
+                spells[currentIndex].gameObject.SetActive(true);
+
+                if (currentIndex <= spells.Length && Input.GetKeyDown(KeyCode.Q))
+                {
+                    spells[currentIndex].gameObject.SetActive(false);
+                    currentIndex = 0;
+                    spells[currentIndex].gameObject.SetActive(true);
+                }
+            }
+        }
+    }
 }
+
 
